e3cf93c2e2ada4a9a8388c6e5ade942afdf8d85e
aliyev-nijat <nicat3699@gmail.com>
2025-12-17 12:32:04 +0400

    Inheritance task 2

---
+++ src/main/java/az/idtech/nijat/inheritance/Employee.java
@@ -0,0 +1,19 @@
+package az.idtech.nijat.inheritance;
+
+public class Employee extends Person{
+
+    private double salary;
+
+    public Employee(String name, int age, double salary) {
+        super(name, age);
+        this.salary = salary;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+}
