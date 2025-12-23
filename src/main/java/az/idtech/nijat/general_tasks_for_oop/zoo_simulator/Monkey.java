8145afe6a6af38b37a71ea344e439cb463057c11
aliyev-nijat <nicat3699@gmail.com>
2025-12-23 21:22:18 +0400

    Zoo Simulator – Inheritance + Polymorphism Description

---
+++ src/main/java/az/idtech/nijat/general_tasks_for_oop/zoo_simulator/Monkey.java
@@ -0,0 +1,9 @@
+package az.idtech.nijat.general_tasks_for_oop.zoo_simulator;
+
+public class Monkey extends Animal {
+
+    @Override
+    public void makeSound() {
+        System.out.println("Ooh ooh aah aah");
+    }
+}
