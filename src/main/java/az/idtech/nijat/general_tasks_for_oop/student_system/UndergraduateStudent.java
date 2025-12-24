f438a0d7b813980b22c90c6f300288eaafdfb245
aliyev-nijat <nicat3699@gmail.com>
2025-12-24 22:34:28 +0400

    Student Grading System – Inheritance + Method Overriding Description

---
+++ src/main/java/az/idtech/nijat/general_tasks_for_oop/student_system/UndergraduateStudent.java
@@ -0,0 +1,9 @@
+package az.idtech.nijat.general_tasks_for_oop.student_system;
+
+public class UndergraduateStudent extends Student{
+
+    @Override
+    public double getFinalGrade() {
+        return super.getFinalGrade();
+    }
+}
