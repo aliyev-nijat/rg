e322c80754c1d4cc16deac5d906a7fdada559f0f
aliyev-nijat <nicat3699@gmail.com>
2025-12-26 13:24:47 +0400

    Create Specific Dates

---
+++ src/main/java/az/idtech/nijat/dec_19/task_2/App.java
@@ -0,0 +1,11 @@
+package az.idtech.nijat.dec_19.task_2;
+
+import java.time.LocalDate;
+
+public class App {
+
+    public static void main(String[] args) {
+        LocalDate birthday = LocalDate.of(2004, 3, 29);
+        System.out.println(birthday);
+    }
+}
